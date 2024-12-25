89aebc04cba468ec977c9de4e64b538294234eab
DERYA GÖKCE <deryagokce40@gmail.com>
2024-12-26 00:43:43 +0300

    Admin panel eklendi

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AracKiralama.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
