7de33f217a5aee3d9d5638aa88e2ab5c2345a95c
DERYA GÖKCE <deryagokce40@gmail.com>
2024-12-26 10:54:34 +0300

    ViewModel eklendi

--- Repositories/CarRepository.cs
+++ Repositories/CarRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AracKiralama.Models;
+using AracKiralama.ViewModels;
 
 namespace AracKiralama.Repositories
 {
@@ -9,38 +10,56 @@ namespace AracKiralama.Repositories
             {
                 _context = context;
             }
-            public List<Car> GetList()
+            public List<CarModel> GetList()
             {
-                var cars = _context.Cars.ToList();
-                return cars;
+                var cars = _context.Cars.Select(x => new CarModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Price = x.Price,
+                    IsActive = x.IsActive
+                }).ToList();
+            return cars;
             }
-            public Car GetById(int id)
+            public CarModel GetById(int id)
             {
-                var car = _context.Cars.Where(s => s.Id == id).FirstOrDefault();
-                return car;
-            }
-            public void Add(Car model)
+            var car = _context.Cars.Where(s => s.Id == id).Select(x => new CarModel()
             {
-                _context.Cars.Add(model);
-                _context.SaveChanges();
-            }
-            public void Update(Car model)
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                IsActive = x.IsActive
+            }).FirstOrDefault(); 
+            return car;
+        }
+            public void Add(CarModel model)
             {
-                var car = GetById(model.Id);
-                if (car != null)
+            var car = new Car()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                IsActive = model.IsActive,
+            };
+            _context.Cars.Add(car); 
+            _context.SaveChanges();
+        }
+        public void Update(CarModel model)
+            {
+                var car = _context.Cars.Where(s => s.Id == model.Id).FirstOrDefault();
+            if (car != null)
                 {
                     car.Id = model.Id;
                     car.Name = model.Name;
                     car.Price = model.Price;
                     car.IsActive = model.IsActive;
                 _context.Cars.Update(car);
-                    _context.SaveChanges();
+                _context.SaveChanges();
                 }
             }
             public void Delete(int id)
             {
-                var car = GetById(id);
-                if (car != null)
+                var car = _context.Cars.Where(s => s.Id == id).FirstOrDefault();
+            if (car != null)
                 {
                     _context.Cars.Remove(car);
                     _context.SaveChanges();
---
+++ ViewModels/CarModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AracKiralama.ViewModels
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Araba Adı")]
+        [Required(ErrorMessage = "Araba Adı Giriniz!")]
+        public string Name { get; set; }
+        [Display(Name = "Fiyat")]
+        [Required(ErrorMessage = "Fiyat Giriniz!")]
+        public int Price { get; set; }
+        [Display(Name = "Mevcut")]
+        public bool IsActive { get; set; }
+    }
+}
